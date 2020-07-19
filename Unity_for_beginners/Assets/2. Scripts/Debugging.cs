54ce8aa79cc56de44948942a3b1b7e7fd928323d
ehdwn4019 <ehdwn7531@gmail.com>
2020-07-19 21:22:02 +0900

    -3강
    -스냅 기능
    -객체 이동
    -디버깅
    -중단점

---
+++ Unity_for_beginners/Assets/2. Scripts/Debugging.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugging : MonoBehaviour
+{
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Score = 90;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Score = 70;
+    }
+
+    void OnMouseDown()
+    {
+        score = score + 1;
+        Debug.Log("마우스를 클릭했습니다 : " + score);
+    }
+}
---
+++ Unity_for_beginners/Assets/2. Scripts/GoBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBack : MonoBehaviour
+{
+    float speed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        v = v * speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * v);
+    }
+}
