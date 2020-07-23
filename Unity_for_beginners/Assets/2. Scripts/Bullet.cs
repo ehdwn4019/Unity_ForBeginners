6d0a89f888f3234ff0591a24b1bb41a005616f5c
ehdwn4019 <ehdwn7531@gmail.com>
2020-07-23 15:56:33 +0900

    -9강
    -Prefab
    -Gizmo 사용
    -총알 생성
    -Instantiate(오브젝트 풀링)
    -Look At 함수

---
+++ Unity_for_beginners/Assets/2. Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float mLastTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mLastTime = Time.time;
+        GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+        if(time-mLastTime>2.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Unity_for_beginners/Assets/2. Scripts/Follow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform firePos;
+    public GameObject bullet;
+
+    Transform tranobj;
+    float mLastTime;
+    RaycastHit hit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tranobj = GameObject.Find("Player").transform;
+        mLastTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(tranobj);
+        float time = Time.time;
+        Debug.DrawRay(firePos.position, firePos.forward * 6, Color.red);
+        if(Physics.Raycast(firePos.position,firePos.forward,out hit,6))
+        {
+            Debug.Log(hit.collider.gameObject.tag);
+            if(time-mLastTime>2.0f)
+            {
+                Instantiate(bullet, firePos.position, firePos.rotation);
+                mLastTime = time;
+            }
+            
+        }
+    }
+}
