43fa7d22b3d6227ef9b90253039ee667c40abf94
ehdwn4019 <ehdwn7531@gmail.com>
2020-07-23 14:40:44 +0900

    -7강
    -카메라
    -SmoothFollow 스크립트
    -카메라 투영 방식
    -OnGUI

---
+++ Unity_for_beginners/Assets/2. Scripts/OnGUITest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGUITest : MonoBehaviour
+{
+    GameObject cube;
+    string str = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if(GUI.RepeatButton(new Rect(80,30,60,40)," ^ "))
+        {
+            str = "위로 이동합니다.";
+            cube.transform.Translate(Vector3.up * 0.1f);
+        }
+        if (GUI.RepeatButton(new Rect(10, 80, 60, 40), " < "))
+        {
+            str = "왼쪽으로 이동합니다.";
+            cube.transform.Translate(Vector3.left * 0.1f);
+        }
+        if (GUI.RepeatButton(new Rect(150, 80, 60, 40), " > "))
+        {
+            str = "오른쪽으로 이동합니다.";
+            cube.transform.Translate(Vector3.right * 0.1f);
+        }
+        if (GUI.RepeatButton(new Rect(80, 130, 60, 40), " v "))
+        {
+            str = "아래로 이동합니다.";
+            cube.transform.Translate(Vector3.down * 0.1f);
+        }
+
+        GUILayout.Label(str);
+    }
+}
