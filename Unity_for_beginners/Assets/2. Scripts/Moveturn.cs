a4b8c5caeb2eda29c6beb04971e22ad386b334dc
ehdwn4019 <ehdwn7531@gmail.com>
2020-07-23 15:00:19 +0900

    -8강
    -Skybox
    -SmoothFollow 스크립트

---
+++ Unity_for_beginners/Assets/2. Scripts/Moveturn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveturn : MonoBehaviour
+{
+    Vector3 forward = new Vector3(0, 0, 1);
+    Vector3 up = new Vector3(0, 1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        v = v * Time.deltaTime;
+        h = h * Time.deltaTime;
+
+        transform.Translate(forward * v * 20);
+        transform.Rotate(up * h * 120);
+    }
+}
